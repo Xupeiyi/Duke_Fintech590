90461dc7d90cc44bf041ea590a826bf6e06dbd2a
px14 <px14@duke.edu>
2022-02-24 21:20:37 -0500

    refactored dist_fit.py

--- febrisk/dist_fit.py
+++ febrisk/dist_fit.py
@@ -1,13 +1,27 @@
-import scipy
+import numpy as np
+import scipy.stats
+import scipy.optimize
 
 
-class MLEDistFitter:
+def normality(data, alpha=0.05):
+    """Examine if data is normally distributed with the Shapiro-Wilk test"""
+    _, p = scipy.stats.shapiro(data)
+    return True if p >= alpha else False 
+
+
+class DistFitter:
     """
     An abstract class to fit the distribution of a random variable x
     using the MLE method.
     
     Users should define the distribution of x in subclasses.
     """
+    constraints = tuple()
+
+    def __init__(self, constraints=None):
+        if constraints is not None:
+            self.constraints = tuple([*self.constraints, *constraints])
+        self.result = None
 
     @staticmethod
     def ll(dist, x):
@@ -25,7 +39,7 @@ class MLEDistFitter:
         """
         raise NotImplementedError
     
-    def fit(self, x, x0, constraints=None, **kwargs):
+    def fit(self, x, x0, **kwargs):
         """Estimate the parameters by maximizing log likelihood function."""
         
         # customize the log-likelihood function and negate it
@@ -39,30 +53,49 @@ class MLEDistFitter:
             dist = self.dist(*args)
             return -self.ll(dist, x)
 
-        all_constraints = getattr(self, 'constraints', {})
-        if constraints:
-            all_constraints.update(constraints)
+        self.result = scipy.optimize.minimize(negated_ll, x0=x0, constraints=self.constraints, **kwargs)
+    
+    @property
+    def fitted_dist(self):
+        return self.dist(*self.result.x)
 
-        return scipy.optimize.minimize(negated_ll, x0=x0, constraints=all_constraints, **kwargs)
-    
-    
-class TFitter(MLEDistFitter):
+    @property
+    def fitted_params(self):
+        return tuple([*self.result.x])
+
+
+class TFitter(DistFitter):
     """
     Fit a t distribution to the data.
     """
-    constraints = ({"type": "ineq", "fun": lambda x: x[1]},  # the degree of freedom of t distribution is positive
-                   {"type": "ineq", "fun": lambda x: x[2]})  # the scale parameter is also positive
+
+    # Inherent Constraints:
+    # the degree of freedom of t distribution must be greater than 2
+    # the scale parameter must be positive
+    constraints = ({"type": "ineq", "fun": lambda x: x[1] - 2},
+                   {"type": "ineq", "fun": lambda x: x[2]})
 
     def dist(self, loc, df, scale):
-        """
-        Assume the data follows a T distribution
-        whose degree of freedom is df and is scaled by scale.
-        """
         return scipy.stats.t(loc=loc, df=df, scale=scale)
+    
+    def fit(self, x, x0=None, **kwargs):
+        x0 = (np.mean(x), 2, np.std(x)) if x0 is None else x0
+        return super().fit(x, x0=x0, **kwargs)
 
 
-class NormalFitter(MLEDistFitter):
+class NormalFitter(DistFitter):
     """
     Fit a normal distribution to the data.
     """
-    pass
+
+    # Inherent Constraints
+    # the standard deviation must be positive
+    constraints = ({"type": "ineq", "fun": lambda x: x[1]},)  
+
+    def dist(self, mean, std):
+        return scipy.stats.norm(loc=mean, scale=std)
+
+    def fit(self, x, x0=None, **kwargs):
+        x0 = (np.mean(x), np.std(x)) if x0 is None else x0
+        return super().fit(x, x0=x0, **kwargs)
+
--- tests/test_dist_fit.py
+++ tests/test_dist_fit.py
@@ -1,6 +1,6 @@
 from unittest import TestCase, main
 import scipy.stats
-from febrisk.dist_fit import TFitter
+from febrisk.dist_fit import TFitter, NormalFitter
 
 
 class TFitterTest(TestCase):
@@ -9,22 +9,77 @@ class TFitterTest(TestCase):
         loc, df, scale = 2, 10, 5
         sim_data = scipy.stats.t(loc=loc, df=df, scale=scale).rvs(100000)
         
-        sim_loc, sim_df, sim_scale = TFitter().fit(x=sim_data, x0=(1, 1, 1)).x
-        self.assertTrue(loc-0.3 < sim_loc < loc+0.3)
+        fitter = TFitter()
+        fitter.fit(x=sim_data)
+        sim_loc, sim_df, sim_scale = fitter.fitted_params
+        self.assertTrue(loc-0.5 < sim_loc < loc+0.5)
         self.assertTrue(df-1 < sim_df < df+1)
-        self.assertTrue(scale-0.2 < sim_scale < scale+0.2)
+        self.assertTrue(scale-0.5 < sim_scale < scale+0.5)
         
     def test_result_is_close_to_scipy_implementation(self):
         loc, df, scale = 1, 7, 9
         sim_data = scipy.stats.t(loc=loc, df=df, scale=scale).rvs(100000)
         
-        sim_loc, sim_df, sim_scale = TFitter().fit(x=sim_data, x0=(1, 1, 1)).x
+        fitter = TFitter()
+        fitter.fit(x=sim_data, x0=(1, 1, 1))
+        sim_loc, sim_df, sim_scale = fitter.fitted_params
         scipy_df, scipy_loc, scipy_scale = scipy.stats.t.fit(sim_data)
         
-        self.assertAlmostEqual(scipy_loc, sim_loc, delta=1e-4)
-        self.assertAlmostEqual(scipy_df, sim_df, delta=1e-4)
-        self.assertAlmostEqual(scipy_scale, sim_scale, delta=1e-4)
+        delta = 5e-4
+        self.assertAlmostEqual(scipy_loc, sim_loc, delta=delta)
+        self.assertAlmostEqual(scipy_df, sim_df, delta=delta)
+        self.assertAlmostEqual(scipy_scale, sim_scale, delta=delta)
+
+    def test_constraints_are_updated(self):
+        loc, df, scale = 10, 7, 9
+        sim_data = scipy.stats.t(loc=loc, df=df, scale=scale).rvs(100000)
+
+        fitter = TFitter(constraints=({"type": "eq", "fun": lambda x: x[0]},))
+        fitter.fit(x=sim_data, x0=(10, 1, 1))
+        sim_loc, _, _ = fitter.fitted_params
+
+        self.assertEqual(0, sim_loc)
+
+    def test_fitter_has_its_own_constraints_when_constructed(self):
+        loc, df, scale = 10, 7, 9
+        sim_data = scipy.stats.t(loc=loc, df=df, scale=scale).rvs(100000)
+
+        fitter1 = TFitter(constraints=({"type": "eq", "fun": lambda x: x[0]-8},))
+        fitter1.fit(x=sim_data, x0=(10, 1, 1))
+        sim_loc, _, _ = fitter1.fitted_params
+        self.assertEqual(8, sim_loc)
+
+        fitter2 = TFitter(constraints=({"type": "eq", "fun": lambda x: x[0]-20},))
+        fitter2.fit(x=sim_data, x0=(10, 1, 1))
+        sim_loc, _, _ = fitter2.fitted_params
+        self.assertEqual(20, sim_loc)
+
+
+class NormalFitterTest(TestCase):
+    
+    def test_parameter_estimation_is_close_to_actual_value(self):
+        loc, scale = 3, 10
+        sim_data = scipy.stats.norm(loc=loc, scale=scale).rvs(100000)
         
+        fitter = NormalFitter()
+        fitter.fit(x=sim_data, x0=(1, 1))
+        sim_loc, sim_scale = fitter.fitted_params
+        self.assertTrue(loc-0.3 < sim_loc < loc+0.3)
+        self.assertTrue(scale-0.2 < sim_scale < scale+0.2)
+        
+    def test_result_is_close_to_scipy_implementation(self):
+        loc, scale = 3, 10
+        sim_data = scipy.stats.norm(loc=loc, scale=scale).rvs(100000)
+        
+        fitter = NormalFitter()
+        fitter.fit(x=sim_data, x0=(1, 1))
+        sim_loc, sim_scale = fitter.fitted_params
+        scipy_loc, scipy_scale = scipy.stats.norm.fit(sim_data)
+        
+        delta = 5e-4
+        self.assertAlmostEqual(scipy_loc, sim_loc, delta=delta)
+        self.assertAlmostEqual(scipy_scale, sim_scale, delta=delta)
+
     
 if __name__ == '__main__':
     main()
