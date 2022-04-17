67601ef1969b2a756528781114b00d2d9bae3472
px14 <px14@duke.edu>
2022-04-17 16:43:19 -0400

    completed cal_risk_attribution

--- febrisk/performance.py
+++ febrisk/performance.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 import numpy as np
+import statsmodels.api as sm
 
 
 def cal_arith_return(prices):
@@ -62,9 +63,43 @@ def cal_return_attribution(weights, returns):
     return:
         - return attribution: np.arrays, shape(n,)
     """
+    assert len(weights) == returns.shape[1], \
+        "weights and returns should have the same number of columns"
     updated_weights = update_weights(weights, returns)
     pfl_returns = (updated_weights * returns).sum(axis=1)
     pfl_total_return = (1 + pfl_returns).prod(axis=0) - 1
     k = np.log(1 + pfl_total_return) / pfl_total_return
     carino_k = np.array([np.log(1 + pfl_returns) / (pfl_returns * k)]).T
     return (returns * updated_weights * carino_k).sum(axis=0)
+
+
+def cal_risk_attribution(weights, returns):
+    """
+    Calculate the risk attribution of each asset in a portfolio given the initial
+    weight and returns. The initial weight and returns starts at the same period.
+
+    params:
+        - weights: np.arrays, shape(n,)
+        - returns: np.arrays, shape(t, n)
+    return:
+        - risk attribution: np.arrays, shape(n,)
+    """
+    assert len(weights) == returns.shape[1], \
+        "weights and returns should have the same number of columns"
+    updated_weights = update_weights(weights, returns)
+    weighted_returns = (updated_weights * returns)
+    pfl_returns = weighted_returns.sum(axis=1)
+    pfl_risk = pfl_returns.std()
+    
+    nassets = len(weights)
+    risk_attribution = np.empty(nassets, dtype=float)
+    for i in range(nassets):
+        # calculate betas
+        # r_i = alpha + sum(beta * r_pfl) + error
+        model = sm.OLS(weighted_returns[:, i], sm.add_constant(pfl_returns))
+        results = model.fit()
+        
+        # sigma_i = beta * sigma_pfl
+        risk_attribution[i] = results.params[1] * pfl_risk
+    
+    return risk_attribution
--- tests/test_performance.py
+++ tests/test_performance.py
@@ -2,7 +2,7 @@ import os
 from unittest import TestCase, main
 import numpy as np
 import pandas as pd
-from febrisk.performance import update_weights, cal_return_attribution
+from febrisk.performance import update_weights, cal_return_attribution, cal_risk_attribution
 from febrisk.math import manhattan_distance
 
 
@@ -30,6 +30,18 @@ class CalReturnAttributionTest(TestCase):
         returns = pd.read_csv(curr_file_dir + "/UpdatedReturns.csv", header=None).values
         result = cal_return_attribution(weights, returns)
         answer = np.array([-0.0046, -0.0071, -0.0037, -0.0075, -0.0022])
+        self.assertAlmostEqual(0, manhattan_distance(answer-result), delta=1e-3)
+
+
+class CalRiskAttributionTest(TestCase):
+    
+    def test_result_is_correct(self):
+        weights = np.array([0.10075988181538116, 0.20950981862533438,
+                            0.43839111238558587, 0.08118475735284336,
+                            0.17015442982085532])
+        returns = pd.read_csv(curr_file_dir + "/UpdatedReturns.csv", header=None).values
+        result = cal_risk_attribution(weights, returns)
+        answer = np.array([0.0016, 0.0032, 0.0048, 0.0009, 0.0014])
         self.assertAlmostEqual(0, manhattan_distance(answer-result), delta=1e-3)
 
 
